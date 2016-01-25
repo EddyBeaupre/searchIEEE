c3fb2e4ba4d68c8ac06a3fac5bcc0fb11c9fd715
Eddy Beaupré <eddy@beaupre.biz>
2016-01-24 22:19:09 -0500

    Add command line version.

---
+++ searchIEEE-Console/IeeeObjects.cs
@@ -0,0 +1,300 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace searchIEEE
+{
+
+    public class IeeeRecord
+    {
+        public String Registry { get; set; }
+        public String Assignment { get; set; }
+        public String Oid
+        {
+            get
+            {
+                try
+                {
+                    Regex rgx = new Regex("[^a-fA-F0-9]");
+                    return (rgx.Replace(Assignment, "").ToUpper().PadRight(12, '0'));
+                }
+                catch
+                {
+                    return ("FFFFFFFFFFFF");
+                }
+            }
+        }
+        public UInt64 Oid64
+        {
+            get
+            {
+                try
+                {
+                    Regex rgx = new Regex("[^a-fA-F0-9]");
+                    return (Convert.ToUInt64("0x" + rgx.Replace(Assignment, "").ToUpper().PadRight(12, '0'), 16));
+                }
+                catch
+                {
+                    return (UInt64.MaxValue);
+                }
+            }
+        }
+        public String OrganizationName { get; set; }
+        public String OrganizationAddress { get; set; }
+        public String Protocol { get; set; }
+    }
+
+    public class IeeeRecords : IDisposable
+    {
+        public IEnumerable<IeeeRecord> data = null;
+        private TextReader textReader = null;
+        private Boolean disposedValue = false;
+
+        public IeeeRecords(String fileName, String Url)
+        {
+            try
+            {
+                if (File.Exists(fileName) == false)
+                {
+                    using (WebClient wc = new WebClient())
+                        File.WriteAllText(fileName, wc.DownloadString(new Uri(Url)));
+                }
+
+                textReader = File.OpenText(fileName);
+                CsvReader csv = new CsvReader(textReader);
+                csv.Configuration.IgnoreHeaderWhiteSpace = true;
+                csv.Configuration.WillThrowOnMissingField = false;
+                data = csv.GetRecords<IeeeRecord>().ToList();
+            }
+            catch
+            {
+                data = null;
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if(textReader != null)
+                        textReader.Close();
+                    textReader = null;
+                    data = null;
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        ~IeeeRecords()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public class ieeeCSV : IDisposable
+    {
+        private bool disposedValue = false;
+
+        private struct databaseInfo
+        {
+            public String fileName;
+            public String Url;
+        }
+
+        private List<IeeeRecords> databases = new List<IeeeRecords>();
+        private List<databaseInfo> dbInfo = new List<databaseInfo>();
+
+        public void Add(String fileName, String Url)
+        {
+            databaseInfo db = new databaseInfo();
+
+            db.fileName = Path.Combine(Path.GetDirectoryName(Application.UserAppDataPath), fileName);
+            db.Url = Url;
+            dbInfo.Add(db);
+        }
+
+        public void loadAll()
+        {
+            foreach (databaseInfo db in dbInfo)
+            {
+                load(db.fileName, db.Url);
+            }
+        }
+
+        public void load(String fileName, String Url)
+        {
+            IeeeRecords db = new IeeeRecords(fileName, Url);
+            if (db != null)
+                databases.Add(db);
+        }
+
+        public void deleteAll()
+        {
+            foreach (IeeeRecords db in databases)
+            {
+                if (db != null)
+                    db.Dispose();
+            }
+            if (databases != null)
+                databases.Clear();
+
+            foreach (databaseInfo db in dbInfo)
+            {
+                if (File.Exists(db.fileName))
+                    File.Delete(db.fileName);
+            }
+        }
+
+        private UInt64? ConvertToOid64(String value)
+        {
+            try
+            {
+                Regex rgx = new Regex("[^a-fA-F0-9]");
+                return (Convert.ToUInt64("0x" + rgx.Replace(value, "").ToUpper().PadRight(12, '0'), 16));
+            }
+            catch
+            {
+                return (null);
+            }
+        }
+
+        public UInt64 Count()
+        {
+            UInt64 Count = 0;
+            if (databases != null)
+            {
+                foreach (IeeeRecords database in databases)
+                {
+                    if(database != null)
+                        if(database.data != null)
+                        Count += (UInt64)database.data.Count();
+                }
+            }
+            return (Count);
+        }
+
+        public List<IeeeRecord> search(String Needle)
+        {
+            try { 
+            List<IeeeRecord> searchResults = new List<IeeeRecord>();
+
+            if (Needle != String.Empty)
+            {
+
+                UInt64? Needle64 = ConvertToOid64(Needle);
+                UInt64[] maskArray = null;
+                if (Needle64 != null)
+                {
+                    UInt64[] array = { (UInt64)0xFFFFFFFFF000 & (UInt64)Needle64, (UInt64)0xFFFFFFFF0000 & (UInt64)Needle64, (UInt64)0xFFFFFFF00000 & (UInt64)Needle64, (UInt64)0xFFFFFF000000 & (UInt64)Needle64, (UInt64)0xFFFFF0000000 & (UInt64)Needle64, (UInt64)0xFFFF00000000 & (UInt64)Needle64, (UInt64)0xFFF000000000 & (UInt64)Needle64, (UInt64)0xFF0000000000 & (UInt64)Needle64 };
+                    maskArray = array;
+                }
+
+
+                foreach (IeeeRecords database in databases)
+                {
+                    foreach (IeeeRecord row in database.data)
+                    {
+                        if (maskArray != null)
+                        {
+                            foreach (UInt64 mask in maskArray)
+                            {
+                                if (mask == row.Oid64)
+                                {
+                                    searchResults.Add(row);
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (row.OrganizationName.IndexOf(Needle, StringComparison.OrdinalIgnoreCase) > -1)
+                        {
+                            searchResults.Add(row);
+                        }
+                        else if (row.OrganizationAddress.IndexOf(Needle, StringComparison.OrdinalIgnoreCase) > -1)
+                        {
+                            searchResults.Add(row);
+                        }
+                        else if (row.Protocol != null)
+                        {
+                            if (row.Protocol.IndexOf(Needle, StringComparison.OrdinalIgnoreCase) > -1)
+                            {
+                                searchResults.Add(row);
+                            }
+                        }
+                    }
+                }
+
+                if (searchResults.Count > 0)
+                {
+                    searchResults.Sort((x, y) => y.Oid64.CompareTo(x.Oid64));
+
+                    return (searchResults);
+                }
+                else {
+                    return (null);
+                }
+            }
+            else
+            {
+                foreach (IeeeRecords database in databases)
+                {
+                    foreach (IeeeRecord row in database.data)
+                    {
+                        searchResults.Add(row);
+                    }
+                }
+                searchResults.Sort((x, y) => x.Oid64.CompareTo(y.Oid64));
+                return (searchResults);
+            }
+            }
+            catch
+            {
+                return (null);
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    foreach (IeeeRecords database in databases)
+                    {
+                        if(database != null)
+                            database.Dispose();
+                    }
+                    if(databases != null)
+                        databases = null;
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        ~ieeeCSV()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ searchIEEE-Console/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Windows.Forms;
+using System.IO;
+using CommandLine;
+using CommandLine.Text;
+using System.Diagnostics;
+
+namespace searchIEEE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                if (options.Search != null)
+                {
+                    search(options.Search);
+                }
+                else if (options.Show == true)
+                {
+                    search(String.Empty);
+                }
+                else
+                    Console.WriteLine(options.GetUsage());
+            }
+            Environment.Exit(0);
+        }
+
+        private static void search(String Needle)
+        {
+            ieeeCSV ieeeCSV = new ieeeCSV();
+            Int64 Count = 0;
+
+            foreach (String file in Directory.GetFiles(Path.GetDirectoryName(Application.UserAppDataPath), @"*.csv"))
+            {
+                ieeeCSV.Add(Path.GetFileName(file), String.Empty);
+            }
+
+            ieeeCSV.loadAll();
+
+            List<IeeeRecord> results = ieeeCSV.search(Needle);
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            if (results != null)
+            {
+                foreach (IeeeRecord record in results)
+                {
+                    Console.WriteLine(Count.ToString() + "," + record.Assignment.Trim() + "," + record.Registry.Trim() + ",\"" + record.OrganizationName.Trim() + "\",\"" + record.OrganizationAddress.Trim() + "\"");
+                    Count++;
+                }
+            }
+        }
+    }
+
+    class Options
+    {
+        [Option('s', "search", HelpText = "Search term.", MutuallyExclusiveSet = "Search")]
+        public string Search { get; set; }
+
+        [Option('a', "all", HelpText = "Show all entries.", MutuallyExclusiveSet = "Search")]
+        public bool Show { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            String name = fvi.ProductName; ;
+            String author = fvi.CompanyName;
+            String version = fvi.FileVersion;
+            String Copyright = fvi.LegalCopyright;
+
+            var help = new HelpText
+            {
+                Heading = new HeadingInfo(name, fvi.FileVersion),
+                Copyright = Copyright,
+                AdditionalNewLineAfterOption = true,
+                AddDashesToOption = true
+            };
+            help.AddPreOptionsLine("Usage: app [-s term] [-a]");
+            help.AddOptions(this);
+            help.AddPostOptionsLine("The databases must be initialized by the GUI version.");
+            return help;
+        }
+    }
+}
